e68fb9b8f005adb7d73dbc0fc9b6e0f074375425
naekang <rlawlsgh6306@gmail.com>
2022-03-30 18:04:44 +0900

    Predicate<T> 사용해보기

--- src/main/java/dev/naekang/java8to11/Foo.java
+++ src/main/java/dev/naekang/java8to11/Foo.java
@@ -2,11 +2,12 @@ package dev.naekang.java8to11;
 
 import java.util.function.Consumer;
 import java.util.function.Function;
+import java.util.function.Predicate;
 import java.util.function.Supplier;
 
 public class Foo {
     public static void main(String[] args) {
-        Supplier<Integer> get10 = () -> 10;
-        System.out.println(get10.get());
+        Predicate<String> startsWithNaekang = (s) -> s.startsWith("naekang");
+        startsWithNaekang.negate();
     }
 }

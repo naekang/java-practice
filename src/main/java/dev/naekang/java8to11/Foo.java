c4c3a1921873c5001af20859e66c8e8c7488e4c9
naekang <rlawlsgh6306@gmail.com>
2022-03-30 17:49:34 +0900

    Functiona<T, R> compose 사용해보기

--- src/main/java/dev/naekang/java8to11/Foo.java
+++ src/main/java/dev/naekang/java8to11/Foo.java
@@ -5,6 +5,10 @@ import java.util.function.Function;
 public class Foo {
     public static void main(String[] args) {
         Function<Integer, Integer> plus10 = (i) -> i + 10;
-        System.out.println(plus10.apply(1));
+        Function<Integer, Integer> multiply2 = (i) -> i * 2;
+
+        Function<Integer, Integer> multiply2AndPlus10 = plus10.compose(multiply2);
+
+        System.out.println(multiply2AndPlus10.apply(2));
     }
 }

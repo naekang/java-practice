94aa1ea2129179b9b0a7d3d3de26b2ac057b1e2c
naekang <rlawlsgh6306@gmail.com>
2022-03-30 18:02:50 +0900

    Supplier<T> 사용해보기

--- src/main/java/dev/naekang/java8to11/Foo.java
+++ src/main/java/dev/naekang/java8to11/Foo.java
@@ -2,10 +2,11 @@ package dev.naekang.java8to11;
 
 import java.util.function.Consumer;
 import java.util.function.Function;
+import java.util.function.Supplier;
 
 public class Foo {
     public static void main(String[] args) {
-        Consumer<Integer> printT = (i) -> System.out.println(i);
-        printT.accept(10);
+        Supplier<Integer> get10 = () -> 10;
+        System.out.println(get10.get());
     }
 }

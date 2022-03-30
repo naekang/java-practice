deec35a5b4db744371e1989ca9b5ff27e9eff6b8
naekang <rlawlsgh6306@gmail.com>
2022-03-30 18:02:27 +0900

    Consumer<T> 사용해보기

--- src/main/java/dev/naekang/java8to11/Foo.java
+++ src/main/java/dev/naekang/java8to11/Foo.java
@@ -1,16 +1,11 @@
 package dev.naekang.java8to11;
 
+import java.util.function.Consumer;
 import java.util.function.Function;
 
 public class Foo {
     public static void main(String[] args) {
-        Function<Integer, Integer> plus10 = (i) -> i + 10;
-        Function<Integer, Integer> multiply2 = (i) -> i * 2;
-
-        Function<Integer, Integer> multiply2AndPlus10 = plus10.compose(multiply2);
-
-        System.out.println(multiply2AndPlus10.apply(2));
-
-        System.out.println(plus10.andThen(multiply2).apply(2));
+        Consumer<Integer> printT = (i) -> System.out.println(i);
+        printT.accept(10);
     }
 }

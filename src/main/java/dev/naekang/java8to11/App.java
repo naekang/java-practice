83bef97e343c441771770327af59e94d0f5db57d
naekang <rlawlsgh6306@gmail.com>
2022-03-31 23:01:27 +0900

    메소드 레퍼런스

--- src/main/java/dev/naekang/java8to11/App.java
+++ src/main/java/dev/naekang/java8to11/App.java
@@ -1,23 +1,12 @@
 package dev.naekang.java8to11;
 
-import java.util.function.Function;
-import java.util.function.Supplier;
-import java.util.function.UnaryOperator;
+import java.util.Arrays;
 
 public class App {
 
     public static void main(String[] args) {
-        Greeting greeting = new Greeting();
-
-        UnaryOperator<String> hello = greeting::hello;
-        hello.apply("nakenag");
-
-        Supplier<Greeting> newGreeting = Greeting::new;
-        newGreeting.get();
-
-        Function<String, Greeting> naekangGreeting = Greeting::new;
-
-        Greeting naekang = naekangGreeting.apply("naekang");
-        System.out.println(naekang.getName());
+        String[] names = {"naekang", "jinho", "toby"};
+        Arrays.sort(names, String::compareToIgnoreCase);
+        System.out.println(Arrays.toString(names));
     }
 }
